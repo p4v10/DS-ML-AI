39b1a5e90b2ec7de3ee904dbd8c9eabc4d8a35a1
Pavlo <71569680+p4v10@users.noreply.github.com>
2023-08-25 23:25:59 -0500

    updating routes

--- rssfeed_reader_ai/routes/__init__.py
+++ rssfeed_reader_ai/routes/__init__.py
@@ -1,13 +1,21 @@
-from flask import redirect, request
+from flask import redirect, request, abort
 from app import app
+from models.articles_summary import ArticlesSummary
+from models.feed_information import FeedInformation
 
-# create a basic get request to allow user input the username
-@app.route('/test', methods=['GET'])
-def test_get():
-    return '<form method="POST"><input name="username"></form>'
+# create a index route
+@app.route('/', methods=['GET'])
+def get_index():
+    return abort(501)
 
-# post request that returns username
-@app.route('/test', methods=['POST'])
-def test_post():
-    username = request.form.get('username', 'no username')
-    return 'Hello ' + username
+@app.route('/read/<article_id>', methods=['GET'])
+def get_read_article(article_id):
+    return abort(501)
+
+@app.route('/sources', methods=['GET'])
+def get_sources():
+    return abort(501)
+
+@app.route('sources', methods=['POST'])
+def post_sources():
+    return abort(501)
