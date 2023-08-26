509f04cb2741faa2207434c4c03c8d5ee3c1cfea
Pavlo <71569680+p4v10@users.noreply.github.com>
2023-08-25 23:11:49 -0500

    Update run.py

--- rssfeed_reader_ai/run.py
+++ rssfeed_reader_ai/run.py
@@ -3,7 +3,6 @@ from db import db
 from models import articles_summary, feed_information
 import routes
 
-
 # this will ensure that all the tables that we create are actually going to be created in the MySQL database
 with app.app_context():
     db.create_all()
