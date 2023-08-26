847d3f141ac19a1ed19323b3053c5b4cdaeffa3c
Pavlo <71569680+p4v10@users.noreply.github.com>
2023-08-25 23:11:20 -0500

    adding form input
    adding form

--- rssfeed_reader_ai/routes/__init__.py
+++ rssfeed_reader_ai/routes/__init__.py
@@ -1,8 +1,13 @@
 from flask import redirect, request
 from app import app
 
+# create a basic get request to allow user input the username
 @app.route('/test', methods=['GET'])
-
 def test_get():
-    return 'Hello from Flask!'
+    return '<form method="POST"><input name="username"></form>'
 
+# post request that returns username
+@app.route('/test', methods=['POST'])
+def test_post():
+    username = request.form.get('username', 'no username')
+    return 'Hello ' + username
