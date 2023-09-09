143bdd9836492b0eebf962df952059e949428e70
Pavlo <71569680+p4v10@users.noreply.github.com>
2023-09-09 18:21:26 -0500

    db fixes

--- rssfeed_reader_ai/app.py
+++ rssfeed_reader_ai/app.py
@@ -4,6 +4,9 @@ from flask import Flask
 app = Flask(__name__)
 
 # configure with database
-db_uri = 'mysql+pymysql://root:password@localhost/rss_articles'
+db_uri = 'mysql+pymysql://root:password@localhost:3306/rss_articles'
 app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+
+# enable query logging
+app.config['SQLALCHEMY_ECHO'] = True
 
